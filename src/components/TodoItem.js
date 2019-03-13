1e15e10f2d77bcc1a08d9279a0b046822ec99b2c
irisEco <549104586@qq.com>
2019-03-13 18:28:56 +0800

    add TodoItem.js

---
+++ src/components/TodoItem.js
@@ -0,0 +1,39 @@
+class TodoItem extends React.Component{
+    //改变任务是否已经完成的状态
+    handlerChange(){
+        let isDone = !this.props.isDone;
+        this.props.changeTodoState(this.props.index,isDone);
+    }
+    //鼠标移入事件
+    handlerMouseOver(){
+        React.findDOMNode(this).style.background = '#eee';
+        React.findDOMNode(this.refs.delButton).style.display = 'inline-block';
+    }
+    //鼠标移出事件
+    handlerMouseOut(){
+        React.findDOMNode(this).style.background = '#fff';
+        React.findDOMNode(this.refs.delButton).style.display = 'none';
+
+    }
+    //删除当前任务
+    handlerDelete(){
+        this.props.delteTodo(this.props.index);
+    }
+    render(){
+        let className = this.props.isDone? 'task-done':'';
+        return(
+            <li onMouseOver={this.handlerMouseOver.bind(this)}
+             onMouseOut={this.handlerMouseOut.bind(this)}>
+             <label>
+                 <input type="checkbox" checked={this.props.isDone} onChange={this.handlerChange.bind(this)} />
+                 <span className={className}>{this.props.text}</span>
+             </label>
+             <button ref = "delbutton" className="btn btn-danger" onclick={this.handlerDelete.bind(this)}>删除</button>
+
+            </li>
+        )
+    }
+
+
+
+}
