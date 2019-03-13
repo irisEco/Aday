ab576d35d671339ed8353cf3bfd10121a677dc6e
irisEco <549104586@qq.com>
2019-03-13 17:56:15 +0800

    add TodoMain.js and more

---
+++ src/components/TodoMain.js
@@ -0,0 +1,22 @@
+class Todo extends React.Component{
+    render(){
+        if(this.props.todos.length == 0){
+            return (
+                <div className="todo-empty">
+                恭喜您,目前没有待办任务!
+                </div>
+            )
+        }else{
+            return (
+                <ul className="todo-main">
+                {
+                    this.props.todos.map((todo,index)=>{
+                        //{...this.props}用来传递todoMain的todos属性和delete,change方法.
+                        return <TodoItem text={todo.text} isDone={todo.isDone} index={index} {...this.props}/>
+                    })
+                }
+                </ul>
+            )
+        }
+    }
+}
